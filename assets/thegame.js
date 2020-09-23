4faab5d1a50078f89245346c2463935ee398112e
Jason R <99057964@mydavinci.nl>
2020-09-23 12:04:01 +0200

    Al 2 levels gemaakt en getest en het werkt.

--- assets/thegame.js
+++ assets/thegame.js
@@ -2,9 +2,39 @@
 //22-09-2020.The game.
 //Da Vinci College. Software Developer.
 
-    const START = "start";//Een constante zodat de start van de game niet kan veranderen!
-    alert('Welcome by the Titanfall game');
-    alert('Your goal is to defeat the boss titan at the end of the 10 levels that you are gonna cross SUCCES!.');
+    function myGame(){//Functie om de game te starten.
+        const START = "start";//Een constante zodat de start van de game niet kan veranderen!
+
+        const YES = "yes";//Een constante omdat de game alleen uit ja of nee bestaat zelfde geld voor de constante onder deze!.
+        const NO = "no";
+    
+        var quistion;//Var voor alle vragen die ik in de levels wil stellen en dan kan ik deze variabele hele tijd veranderen.
+    
+        alert('Welcome by the Titanfall game');//Titel.
+        alert('Jouw doel is om de eind baas titan te verslaan door jou eigen titan waar je nu in zit met wapens te bemachtigen die random de 10 levels zitten maar er zijn ook gevaren tijdens de 10 levels dus SUCCES!');//Intro voor de game.
+
+        quistion = prompt('Ga jij je titan activeren?');//De eerste level vraag.
+        if(quistion == YES){//Level 1.
+            alert('Goede keus. Nu kan je door naar het volgende level');//Alert omdat het antwoord goed is. En waarom het goed is.
+            quistion = quistion2 = prompt('Welkom by level 2: Je ziet een minigun op de grond liggen pak jij de minigun op?');//Vraag voor level 2.
+                if(quistion2 == YES){//Level 2.
+                    alert('Goed dat je de minigun hebt opgepakt zo kan jij jezelf beschermen.');//Alert omdat het antwoord goed is. En waarom het goed is.
+                    
+                } else {//Voor als fout geantwoord is en waarom het fout is.
+                    alert('Je kan helaas niet door naar het volgende level, omdat je de mini gun nodig hebt om jezelf te beschermen.');
+                }
+        } else{//Voor als fout geantwoord is en waarom het fout is.
+            alert('Je kan helaas niet door naar het volgende level omdat jij je titan nodig hebt!');
+        }
+
+    }
+
+    console.log(myGame());
+      
+
+
+
+
 
 
 
