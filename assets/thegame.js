7435bfae32aa4227d5c2e573f45234a1a04b1113
Jason R <99057964@mydavinci.nl>
2020-09-23 10:48:10 +0200

    begin gemaakt met de game.

--- assets/thegame.js
+++ assets/thegame.js
@@ -1,6 +1,14 @@
 //Jason Ronoastro.
-//22-09-2020. The game.
+//22-09-2020.The game.
 //Da Vinci College. Software Developer.
+
+    const START = "start";//Een constante zodat de start van de game niet kan veranderen!
+    alert('Welcome by the Titanfall game');
+    alert('Your goal is to defeat the boss titan at the end of the 10 levels that you are gonna cross SUCCES!.');
+
+
+
+
 
 
 
